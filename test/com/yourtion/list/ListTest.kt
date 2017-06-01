981dc591ff149ca7a061600ea5ea14b7db98c310
YourtionGuo <yourtion@gmail.com>
2017-06-01 16:37:44 +0800

    add List Example

--- test/com/yourtion/list/ListTest.kt
+++ test/com/yourtion/list/ListTest.kt
@@ -52,4 +52,82 @@ internal class ListTest {
         assertTrue(list.is_tail(b))
     }
 
+    fun List.print() {
+        if (size == 0) return println("-> List is Empty")
+        var str = "-> List size: $size \n-"
+        var element = head
+        while (element != null) {
+            str += "-> [" + element.data.toString() + "] "
+            element = element.next
+        }
+        str += "\n"
+        print(str)
+    }
+
+    @Test
+    fun example() {
+        val list = List()
+
+        for (i in IntRange(1, 10).reversed()) {
+            list.insert_next(i)
+        }
+        list.print()
+        assertEquals(list.size, 10)
+
+        var element = list.head
+        for (i in IntRange(0, 6)) {
+            element = element!!.next
+        }
+        println("Removing an element after the one containing " + element!!.data)
+        assertEquals(element.data, 8)
+        element = list.remove_next(element)
+        list.print()
+        assertEquals(element!!.data, 9)
+
+        println("Inserting 11 at the tail of the list")
+        list.insert_next(11, list.tail)
+        list.print()
+        assertEquals(list.tail!!.data, 11)
+
+        println("Removing an element after the first element")
+        element = list.remove_next(list.head)
+        list.print()
+        assertEquals(element!!.data, 2)
+
+        println("Inserting 12 at the head of the list")
+        list.insert_next(12)
+        list.print()
+        assertEquals(list.head!!.data, 12)
+
+        println("Iterating and removing the fourth element")
+        element = list.head
+        element = element!!.next
+        element = element!!.next
+        element = list.remove_next(element)
+        list.print()
+        assertEquals(element!!.data, 4)
+
+        println("Inserting 13 after the first element")
+        list.insert_next(13, list.head)
+        list.print()
+        assertEquals(list.size, 10)
+
+        var i = list.is_head(list.head!!)
+        println("Testing is_head...Value=$i (true=OK)")
+        assertTrue(i)
+
+        i = list.is_head(list.tail!!)
+        println("Testing is_head...Value=$i (false=OK)")
+        assertFalse(i)
+
+        i = list.is_tail(list.tail!!)
+        println("Testing is_tail...Value=$i (true=OK)")
+        assertTrue(i)
+
+        i = list.is_tail(list.head!!)
+        println("Testing is_tail...Value=$i (false=OK)")
+        assertFalse(i)
+
+    }
+
 }

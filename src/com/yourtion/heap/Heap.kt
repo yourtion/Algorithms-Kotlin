7942dc1be2e63f316eb7bfacac6099dc55d35cb7
YourtionGuo <yourtion@gmail.com>
2017-06-08 11:12:29 +0800

    add Heap Comment

--- src/com/yourtion/heap/Heap.kt
+++ src/com/yourtion/heap/Heap.kt
@@ -17,22 +17,36 @@ class Heap constructor(compare: (Any, Any) -> Int) {
         this[index2] = tmp
     }
 
+    /**
+     * 获取父结点位置 (i-1)/2
+     */
     private fun parent(pos: Int): Int {
         return (pos - 1) / 2
     }
 
+    /**
+     * 获取左子结点位置 i*2+1
+     */
     private fun left(pos: Int): Int {
         return pos * 2 + 1
     }
 
+    /**
+     * 获取右子结点位置 i*2+2
+     */
     private fun right(pos: Int): Int {
         return pos * 2 + 2
     }
 
+    /**
+     * 向堆中插入一个结点 [data]
+     */
     fun insert(data: Any) {
         var ipos = size
         var ppos = parent(ipos)
         tree.add(element = data)
+
+        // 通过将节点上推保障树的平衡
         while (ipos > 0 && compare(tree[ppos], tree[ipos]) < 0) {
             tree.swap(ppos, ipos)
             ipos = ppos
@@ -40,10 +54,15 @@ class Heap constructor(compare: (Any, Any) -> Int) {
         }
     }
 
+    /**
+     * 从堆中释放堆顶部的结点
+     */
     fun extract(): Any? {
         if (size < 1) return null
         val data = tree.removeAt(0)
         if (size == 0) return data
+
+        // 通过将顶部节点下移保障树的平衡
 
         var ipos = 0
         var lpos : Int
@@ -64,6 +83,7 @@ class Heap constructor(compare: (Any, Any) -> Int) {
                 mpos = rpos
             }
 
+            // 当 mpos 就是 ipos 时，堆已经完成调整
             if (mpos == ipos) {
                 break
             } else {

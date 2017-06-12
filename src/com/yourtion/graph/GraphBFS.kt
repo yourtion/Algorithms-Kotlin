68ccf37bd8f90da46372bc1b663ddfd2e2faabc1
YourtionGuo <yourtion@gmail.com>
2017-06-12 17:15:15 +0800

    GraphBFS use Iterable

--- src/com/yourtion/graph/GraphBFS.kt
+++ src/com/yourtion/graph/GraphBFS.kt
@@ -21,10 +21,9 @@ data class BFSVertex<E>(
  * 从 start 开始对图执行广度搜索
  */
 fun <E> Graph<BFSVertex<E>>.bfs(start: BFSVertex<E>): List<BFSVertex<E>>? {
-    var element = adjlists.head
     var clr_vertex: BFSVertex<E>?
-    while (element != null) {
-        clr_vertex = element.data.vertex
+    for ((vertex) in adjlists) {
+        clr_vertex = vertex
         if (clr_vertex.data == start.data) {
             clr_vertex.color = VertexColor.Gray
             clr_vertex.hops = 0
@@ -32,7 +31,6 @@ fun <E> Graph<BFSVertex<E>>.bfs(start: BFSVertex<E>): List<BFSVertex<E>>? {
             clr_vertex.color = VertexColor.White
             clr_vertex.hops = -1
         }
-        element = element.next
     }
 
     val queue = Queue<AdjList<BFSVertex<E>>>()
@@ -40,16 +38,12 @@ fun <E> Graph<BFSVertex<E>>.bfs(start: BFSVertex<E>): List<BFSVertex<E>>? {
     queue.enqueue(clr_adjlist)
 
     var adjlist: AdjList<BFSVertex<E>>?
-    var adj_vertex: BFSVertex<E>?
-    var element_temp : List.ListElmt<BFSVertex<E>>?
     while (queue.size > 0) {
 
         adjlist = queue.peek()
 
-        element_temp = adjlist!!.adjacent.head
-        while (element_temp != null) {
-            adj_vertex = element_temp.data
-            clr_adjlist = get_adjlist(adj_vertex) ?: return null
+        for (element in adjlist!!.adjacent) {
+            clr_adjlist = get_adjlist(element) ?: return null
             clr_vertex = clr_adjlist.vertex
 
             if (clr_vertex.color == VertexColor.White) {
@@ -57,23 +51,18 @@ fun <E> Graph<BFSVertex<E>>.bfs(start: BFSVertex<E>): List<BFSVertex<E>>? {
                 clr_vertex.hops = adjlist.vertex.hops + 1
                 queue.enqueue(clr_adjlist)
             }
-            element_temp = element_temp.next
         }
 
         adjlist = queue.dequeue()
         adjlist!!.vertex.color = VertexColor.Black
     }
-    
+
     val list = List<BFSVertex<E>>()
-    element = adjlists.head
-    while (element != null) {
-        clr_vertex = element.data.vertex
-        if (clr_vertex.hops != -1) {
-            list.insert_next(clr_vertex, list.tail)
-        }
-        element = element.next
-    }
+    adjlists
+            .filter { it.vertex.hops != -1 }
+            .forEach { (vertex) ->
+                list.insert_next(vertex, list.tail)
+            }
 
     return list
 }
-

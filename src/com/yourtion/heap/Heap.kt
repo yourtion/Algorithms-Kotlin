b575ed31713b825caf9245108a2a5b7a91e07811
YourtionGuo <yourtion@gmail.com>
2017-06-13 12:04:42 +0800

    add utils Extensions

--- src/com/yourtion/heap/Heap.kt
+++ src/com/yourtion/heap/Heap.kt
@@ -1,5 +1,7 @@
 package com.yourtion.heap
 
+import com.yourtion.utils.swap
+
 /**
  * Heap
  * Created by Yourtion on 07/06/2017.
@@ -14,12 +16,6 @@ open class Heap<E> constructor(comparator: Comparator<E>) {
     private var tree: MutableList<E> = mutableListOf()
     val size: Int
         get() = tree.size
-
-    fun <T> MutableList<T>.swap(index1: Int, index2: Int) {
-        val tmp = this[index1]
-        this[index1] = this[index2]
-        this[index2] = tmp
-    }
 
     /**
      * 获取父结点位置 (i-1)/2
---
+++ src/com/yourtion/utils/Extensions.kt
@@ -0,0 +1,16 @@
+package com.yourtion.utils
+
+/**
+ * Created by Yourtion on 13/06/2017.
+ */
+
+fun <T> MutableList<T>.swap(index1: Int, index2: Int) {
+    val tmp = this[index1]
+    this[index1] = this[index2]
+    this[index2] = tmp
+}
+
+
+
+
+

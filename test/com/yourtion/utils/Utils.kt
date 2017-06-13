a79d2fc80ccb4f7467ecfdf0c9e1bd179384c69b
YourtionGuo <yourtion@gmail.com>
2017-06-13 12:05:24 +0800

    add test utils

---
+++ test/com/yourtion/utils/Utils.kt
@@ -0,0 +1,25 @@
+package com.yourtion.utils
+
+import com.yourtion.list.DList
+import com.yourtion.list.List
+import org.junit.Assert.assertEquals
+
+/**
+ * Created by Yourtion on 13/06/2017.
+ */
+
+val compare_int = { data1: Int, data2: Int -> data1 - data2 }
+val compare_string = { data1: String, data2: String -> data1.compareTo(data2) }
+
+fun <E> verify(list: List<E>, res: Array<E>) {
+    for ((index, data) in list.withIndex()) {
+        assertEquals(data, res[index])
+    }
+}
+
+fun <E> verify(list: DList<E>, res: Array<E>) {
+    for ((index, data) in list.withIndex()) {
+        assertEquals(data, res[index])
+    }
+}
+
